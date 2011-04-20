5c147fc07948f19866fdd01559cee18ab6e76398
NoWall <i@nowall.be>
2011-04-20 23:38:07 +0800

    add decodeQuery decodePathAndQuery

--- tests/test-encode.js
+++ tests/test-encode.js
@@ -1,6 +1,10 @@
-encode = require('../lib/encode');
+options = {serverAndPort:'posts.li'}
+encode = require('../lib/encode')(options);
 fs = require('fs');
 
+console.log(encode.encodeBody(fs.readFileSync('./phoenix_plugins.bundle.js', 'utf8'), true, true));
+console.log('\n============bigfile tests done==========');
+console.log('\n============test encodeHost==========');
 console.log(encode.encodeHost('twitter.com'));
 console.log(encode.encodeHost('http://www.twitter.com'));
 console.log(encode.encodeHost('http://images.facebook.com/asdfa'));
@@ -9,6 +13,7 @@ console.log(encode.encodeHost('http://groups.goo-gle.com/asdfa'));
 console.log(encode.encodeHost('http://groups.google.com.hk/asdfa'));
 console.log(encode.encodeHost('http://groups.goo-gle.com.hk/asdfa'));
 
+console.log('\n============test decodeHost==========');
 console.log(encode.decodeHost(encode.encodeHost('twitter.com')));
 console.log(encode.decodeHost(encode.encodeHost('http://www.twitter.com')));
 console.log(encode.decodeHost(encode.encodeHost('http://images.facebook.com/asdfa')));
@@ -17,10 +22,15 @@ console.log(encode.decodeHost(encode.encodeHost('http://groups.goo-gle.com/asdfa
 console.log(encode.decodeHost(encode.encodeHost('http://groups.google.com.hk/asdfa')));
 console.log(encode.decodeHost(encode.encodeHost('http://groups.goo-gle.com.hk/asdfa')));
 
-options = {serverAndPort:'posts.li'}
-console.log(encode.encodeScript('domain.xx="twitter.com";', options));
-console.log(encode.encodeScript('domain.xx="www.google.com.hk";', options));
-console.log(encode.encodeScript('domain.xx="www.goo-gle.com.hk";', options));
-console.log(encode.encodeScript('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) ' , options))
-console.log(encode.encodeBody('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) ' , options, true))
-console.log(encode.encodeBody(fs.readFileSync('./phoenix_plugins.bundle.js', 'utf8'), options, true, true));
+console.log('\n============test encodeScript and body==========');
+console.log(encode.encodeScript('domain.xx="twitter.com";'));
+console.log(encode.encodeScript('domain.xx="www.google.com.hk";'));
+console.log(encode.encodeScript('domain.xx="www.goo-gle.com.hk";'));
+console.log(encode.encodeScript('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) '))
+console.log(encode.encodeBody('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) ', true))
+
+console.log('\n============test decodeQuery==========');
+console.log(encode.decodeQuery('pstMsg=&dnConn=&continue=http%3A%2F%2Fwww.qyyqvo-myw.posts.li%2F&dsh=4529719864886304822&hl=zh-CN&timeStmp=&secTok=&GALX=x2eIIz7c7Oo&Email=gg&Passwd=gg&PersistentCookie=yes&rmShown=1&signIn=%E7%99%BB%E5%BD%95&asts='))
+
+console.log('\n============test decodePathAndQuery==========');
+console.log(encode.decodePathAndQuery('/account/auth/?pstMsg=&dnConn=&continue=http%3A%2F%2Fwww.qyyqvo-myw.posts.li%2F&dsh=4529719864886304822&hl=zh-CN&timeStmp=&secTok=&GALX=x2eIIz7c7Oo&Email=gg&Passwd=gg&PersistentCookie=yes&rmShown=1&signIn=%E7%99%BB%E5%BD%95&asts='))
