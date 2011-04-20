816a278c9e81c64043eb8609ee30e00e58efbb2f
NoWall <i@nowall.be>
2011-04-21 00:08:30 +0800

    fix a problem of facebook

--- tests/test-encode.js
+++ tests/test-encode.js
@@ -29,6 +29,7 @@ console.log(encode.encodeScript('domain.xx="www.goo-gle.com.hk";'));
 console.log(encode.encodeScript('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) '))
 console.log(encode.encodeBody('"api":"http:\\/\\/a2.twimg.com\\/a\\/1302888170\\/javascripts\\/api.bundle.js", hostname.match(/(^(www|api)\\.)?twitter\\.com$/) ', true))
 
+console.log(encode.encodeBody('<link type="text/css" rel="stylesheet" href="http://static.ak.fbcdn.net/rsrc.php/v1/y2/r/8Sr4ddHR5zZ.css" />'));
 console.log('\n============test decodeQuery==========');
 console.log(encode.decodeQuery('pstMsg=&dnConn=&continue=http%3A%2F%2Fwww.qyyqvo-myw.posts.li%2F&dsh=4529719864886304822&hl=zh-CN&timeStmp=&secTok=&GALX=x2eIIz7c7Oo&Email=gg&Passwd=gg&PersistentCookie=yes&rmShown=1&signIn=%E7%99%BB%E5%BD%95&asts='))
 
