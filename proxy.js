29701d4c283d296f660722efbda2a6a3171c3552
NoWall <i@nowall.be>
2011-05-05 00:03:31 +0800

    fix typo

--- proxy.js
+++ proxy.js
@@ -1,20 +1,23 @@
-var config, connect, proxy;
 require.paths.unshift(__dirname + "/support/");
-connect = require('connect');
-config = require('./config');
+
+var connect = require('connect'),
+    config = require('./config');
+
 config.useHttps = config.useHttps && true;
 config.port = config.port || (config.useHttps ? 443 : 80);
-proxy = global.proxy = require('myproxy')({
+
+var proxy = global.proxy = require('myproxy')({
     server: config.server,
     port: config.port,
     useHttps: config.useHttps,
     compress: config.compress || false,
     logger: config.logger
 });
+
 module.exports = connect(function(req, res) {
     try {
       return proxy(req, res);
     } catch (e) {
-      return logger.error('error to handle proxy of ' + req.headers.host + req.url, e);
+      return config.logger.error('error to handle proxy of ' + req.headers.host + req.url, e);
     }
 });
