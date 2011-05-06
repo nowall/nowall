e9da190e7f637373b1bf1ee303fe358dddf4e534
NoWall <i@nowall.be>
2011-05-06 22:41:58 +0800

    add sign up page

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express'),
     config = require('./config'),
     querystring = require('querystring'),
+    db = require('./database'),
     app = module.exports = express.createServer();
 
+var RedisStore = require('connect-redis');
 app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "sexy girls", store: new RedisStore }));
     //app.use(require('nothing').middleware({ src: __dirname + '/public' }));
     app.use(require('stylus').middleware({
           src: __dirname + '/public'
     }));
     app.use(app.router);
-    return app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function() {
@@ -27,31 +31,75 @@ app.configure('production', function() {
     return app.use(express.errorHandler());
 });
 
+app.dynamicHelpers({
+    messages : require('express-messages')
+});
+
 app.helpers({
     config : config
 });
 
 app.get('/', function(req, res) {
-    return res.redirect('/here');
+    res.redirect('/here');
 });
 
 app.get('/here', function(req, res) {
-    return res.render('index', {
+    res.render('index', {
         title: 'No WALL be here'
     });
 });
 
 app.post('/donation/success', function(req, res) {
-    return res.render('donation_success', {
+    res.render('donation_success', {
         title: 'Thank you',
         data: req.body
-      });
+    });
 });
 
 app.post('/donation/cancel', function(req, res) {
-    return res.render('donation_cancel', {
+    res.render('donation_cancel', {
         title: 'Thank you',
         data: req.body
+    });
+});
+
+app.get('/signup', function(req, res) {
+    var email = req.query.email,
+        verify_code = req.query.verify_code;
+
+    db.user.findOne({email: email, verify_code: verify_code}, function(err, result){
+        if(!result){
+          res.render('404',function(err,result){
+              res.send(result, 404);
+          });
+        } else {
+          res.render('signup', {
+              user : result
+          });
+        }
+    });
+});
+
+app.post('/signup', function(req, res) {
+    var email = req.body.email,
+        verify_code = req.body.verify_code;
+    console.dir(req.body);
+
+    db.user.update({email: email, verify_code: verify_code},
+      {
+        $set: {password: req.body.password},
+        $unset: {verify_code: 1}
+      },
+      function(err, reply){
+        console.dir(reply);
+        if(err){
+          res.render('500',function(err, result){
+              res.send(result, 500)
+          });
+        } else {
+          req.flash('info', '注册成功')
+          res.redirect('/');
+        }
     });
 });
 
