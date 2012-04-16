41d3d17e087a72cf50c41122f2d9e59c31c012a6
NoWall <i@nowall.be>
2012-04-16 13:00:37 +0800

    fix twitter secure

--- plugin/twitter.js
+++ plugin/twitter.js
@@ -1,6 +1,6 @@
 var exports = module.exports = function(req, res, sreq, sres, next){
   if(req.host.indexOf('twitter.com') >=0) {
-    res.body = res.body.replace("document.domain = 'twitter.com';", '');
+    res.body = res.body.replace(/document.domain\s*=\s*['\"].*['"];?/g, '');
   }
   next();
 }
