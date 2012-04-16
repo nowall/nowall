f2a4d793761c7effddf1f9ec433e2aecdf8f417d
NoWall <i@nowall.be>
2012-04-16 12:40:08 +0800

    fix twitter.js

--- plugin/twitter.js
+++ plugin/twitter.js
@@ -1,5 +1,5 @@
 var exports = module.exports = function(req, res, sreq, sres, next){
-  if(req.host == 'twitter.com') {
+  if(req.host.indexOf('twitter.com') >=0) {
     res.body = res.body.replace("document.domain = 'twitter.com';", '');
   }
   next();
