79cb411f214ff5c67464f42d2faf4c3b43bf5fb2
NoWall <i@nowall.be>
2012-04-16 11:24:20 +0800

    first commit twitter.com

--- plugin/twitter.js
+++ plugin/twitter.js
@@ -1,3 +1,6 @@
 var exports = module.exports = function(req, res, sreq, sres, next){
+  if(req.host == 'twitter.com') {
+    res.body = res.body.replace("document.domain = 'twitter.com';", '');
+  }
   next();
 }
