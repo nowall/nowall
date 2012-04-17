838d039d8bddcfb16e97e69b71dbfb9254e523bd
NoWall <i@nowall.be>
2012-04-17 10:18:37 +0800

    fix twitter API endpoints again

--- plugin/twitter.js
+++ plugin/twitter.js
@@ -4,15 +4,15 @@ var exports = module.exports = function(req, res, sreq, sres, next){
   }
   else if(req.host.indexOf('akamaihd.net') >= 0) {
     // c/phoenix/en/bundle/t1-hogan-core.f760c184d1eaaf1cf27535473a7306ef.js
+    // ,d=twttr.anywhere.api.endpoints[this.methodName];
     //if(!d)throw new Error("The endpoint, "+this.methodName+" is not a registered API method");
     res.body = res.body.replace(
-      'if(!d)throw new Error("The endpoint, "+this.methodName+" is not a registered API method");', 
+      'd=twttr.anywhere.api.endpoints[this.methodName];'
+      // 'if(!d)throw new Error("The endpoint, "+this.methodName+" is not a registered API method");'
       //  The endpoint, account/verify_credentials?px!=https://twimg0-a.akamaihd.net is not a registered API method
-      'if(!d) {' +
-      'if(d.match(/.*px!=.*/))' + 
-      'd = d.replace(/.px!=.*$/, "");' +
-      'else throw new Error("The endpoint, "+this.methodName+" is not a registered API method");' +
-      '}')
+    , 'if(this.methodName.match(/.*px!=.*/)) ' + 
+      'this.methodName = this.methodName.replace(/.px!=.*$/, "");' +
+      'd=twttr.anywhere.api.endpoints[this.methodName];')
   }
   next();
 }
