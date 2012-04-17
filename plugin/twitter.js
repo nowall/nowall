239d1f1c70cf70c2dcf43b83a3c90c56afaecb3d
NoWall <i@nowall.be>
2012-04-17 10:08:44 +0800

    fix twitter endpoint API

--- plugin/twitter.js
+++ plugin/twitter.js
@@ -1,6 +1,18 @@
 var exports = module.exports = function(req, res, sreq, sres, next){
   if(req.host.indexOf('twitter.com') >=0) {
-    res.body = res.body.replace(/document.domain\s*=\s*['"].*['"];?/g, '');
+    res.body = res.body.replace(/document.domain\s*=\s*['"].*['"];?/ig, '');
+  }
+  else if(req.host.indexOf('akamaihd.net') >= 0) {
+    // c/phoenix/en/bundle/t1-hogan-core.f760c184d1eaaf1cf27535473a7306ef.js
+    //if(!d)throw new Error("The endpoint, "+this.methodName+" is not a registered API method");
+    res.body = res.body.replace(
+      'if(!d)throw new Error("The endpoint, "+this.methodName+" is not a registered API method");', 
+      //  The endpoint, account/verify_credentials?px!=https://twimg0-a.akamaihd.net is not a registered API method
+      'if(!d) {' +
+      'if(d.match(/.*px!=.*/))' + 
+      'd = d.replace(/.px!=.*$/, '');' +
+      'else throw new Error("The endpoint, "+this.methodName+" is not a registered API method");' +
+      '}')
   }
   next();
 }
