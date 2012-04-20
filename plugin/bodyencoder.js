62b5d2f78a623e4ef4a4b4d84f0debba9ffa9eb2
NoWall <i@nowall.be>
2012-04-20 09:48:37 +0800

    not encode script at all

--- plugin/bodyencoder.js
+++ plugin/bodyencoder.js
@@ -1,6 +1,11 @@
 exports.bodyEncoder  = function(creq, cres, sreq, sres, next, logger) {
   var encoder = sreq.encoder;
-  var body = encoder.encodeBody(cres.body, cres.isScript, cres.isStyle);
+  // TODO allow options to load script but not encode script
+  if(cres.isScript) {
+    var body = cres.body;
+  } else {
+    var body = encoder.encodeBody(cres.body, cres.isScript, cres.isStyle);
+  }
   sres.write(new Buffer(body, 'binary'));
   sres.end();
 }
