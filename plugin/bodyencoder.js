729b287bf6966478d051cb991474b7a2d9544a1b
NoWall <i@nowall.be>
2012-04-20 10:37:10 +0800

    fix twitter home page

--- plugin/bodyencoder.js
+++ plugin/bodyencoder.js
@@ -2,6 +2,9 @@ exports.bodyEncoder  = function(creq, cres, sreq, sres, next, logger) {
   var encoder = sreq.encoder;
   // TODO allow options to load script but not encode script
   var body = encoder.encodeBody(cres.body, cres.isScript, cres.isStyle);
+  if(creq.host.indexOf('twitter.com') >= 0) {
+    body = body.replace('href="/?px!=https:twitter.com#!/"', 'href="/#!/"');
+  }
   sres.write(new Buffer(body, 'binary'));
   sres.end();
 }
