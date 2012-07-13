732cd32333ab4a20b0ca18633cdec6ccb9726d8c
NoWall <i@nowall.be>
2012-07-13 18:20:57 +0800

    auto restart child process

--- cluster.js
+++ cluster.js
@@ -1,17 +1,48 @@
 var cluster = require('cluster')
-  , numCPUs = require('os').cpus().length;
+  , os = require('os')
+  , freemem = os.freemem
+  , numCPUs = os.cpus().length;
+
+var MEGA = 1024 * 1024
+  , MIN_FREE_MEM = 30 * MEGA
+  ;
+
+if(process.version < 'v0.8') {
+  console.error('require nodejs v0.8');
+  process.exit();
+}
 
 if(cluster.isMaster) {
-  // Fork workers
-  for(var i=0; i< numCPUs; i++) {
-    cluster.fork();
+  function startWorkers(){
+    console.log('startWorkers');
+    // Fork workers
+    for(var i=0; i< numCPUs; i++) {
+      cluster.fork();
+    }
   }
+
+  function stopWorkers() {
+    console.log('stopWorkers');
+    for(var id in cluster.workers) {
+      cluster.workers[id].destroy();
+    }
+  }
+
+  startWorkers();
 
   cluster.on('death', function(worker){
       // TODO log it in another logfile
       console.log('worker ' + worker.pid + ' died');
       // cluster.fork();
   });
+
+  // for every 10 seconds, check free memory, if too low, respawn workers
+  setInterval(function(){
+      if(freemem() < MIN_FREE_MEM) {
+        stopWorkers();
+        startWorkers();
+      }
+  }, 10000);
 } else {
   require('./server');
 }
