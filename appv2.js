5a9ad687050f57ee7f2d8e89435eff85ef9ae921
NoWall <i@nowall.be>
2012-07-14 12:20:53 +0800

    fix v2

--- appv2.js
+++ appv2.js
@@ -111,8 +111,8 @@ app.post('/signup', function(req, res) {
 
 app.get('/search', function(req, res) {
         var m, q, url, _ref;
-        q = utils.decodeX(q);
         q = req.query.q.trim();
+        q = utils.decodeX(q);
         m = q.match(/^(https?:\/\/)?([\w\d][\.\w\d\-]+\.(\w{2,4})(\.\w{2})?\/?\S*)$/);
         if (m && (m[1] || ((_ref = m[3]) === 'com' || _ref === 'org' || _ref === 'edu' || _ref === 'net'))) {
           url = (m[1] || 'http://') + m[2];
