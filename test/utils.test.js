cd2bd6d7bb25271254a45aad8310a384cc5f5e31
NoWall <i@nowall.be>
2012-07-15 11:22:38 +0800

    fix previous commit

--- test/utils.test.js
+++ test/utils.test.js
@@ -38,6 +38,8 @@ describe('utils', function(){
         // console.log(utils.encodeOld('com|net|org|edu|info|tv|us|tw|jp|ko|in'))
         getOlderHost('www.ozymr3swo2-myw-36.nowall.be', serverPort).should.equal('www.epochtimes.com.tw')
         getOlderHost('www.ozymr3swo2-myw.nowall.be', serverPort).should.equal('www.epochtimes.com')
+        getOlderHost('sub.sub.ozymr3swo2-myw.nowall.be', serverPort).should.equal('sub.sub.epochtimes.com')
+        getOlderHost('ozymr3swo2-myw.nowall.be', serverPort).should.equal('epochtimes.com')
         should.not.exist(getOlderHost('www.ozymr3swo2-s0m.nowall.be', serverPort))
     })
 })
