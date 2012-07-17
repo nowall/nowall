028eb0a9a9b0aeb631040662c4fea5a657ed7e12
NoWall <i@nowall.be>
2012-07-18 02:14:30 +0800

    fix youtube flv url again

--- plugin/youtube.js
+++ plugin/youtube.js
@@ -38,7 +38,7 @@ exports.replacePlayer = function(body, player) {
       if(!flashvars) return full;
       console.log(flashvars);
       var flvurl = flashvars.url_encoded_fmt_stream_map.url;
-      flashvars.flvurl = 'https://ssl.nowall.be/' + utils.encodeSymboUrl(flvurl);
+      flashvars.flvurl = 'https://ssl.nowall.be' + utils.encodeSymboUrl(flvurl);
       return template.render(player, flashvars);
   });
 };
