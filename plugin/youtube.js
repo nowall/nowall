60b02b5e5a25c4d491e49840aec7a6e480b6ab70
NoWall <i@nowall.be>
2012-07-18 12:50:01 +0800

    no more encoding for flv

--- plugin/youtube.js
+++ plugin/youtube.js
@@ -37,8 +37,8 @@ exports.replacePlayer = function(body, player, vid) {
       var flashvars = exports.stripFlashvars(script);
       if(!flashvars) return full;
       console.log(flashvars);
-      var flvurl = flashvars.url_encoded_fmt_stream_map.url;
-      flashvars.flvurl = 'https://ssl.nowall.be' + utils.encodeSymboUrl(flvurl);
+      flashvars.flvurl = flashvars.url_encoded_fmt_stream_map.url;
+      // flashvars.flvurl = 'https://ssl.nowall.be' + utils.encodeSymboUrl(flashvars.flvurl);
       flashvars.vid = vid;
       flashvars.thumbnail = 'https://i3.ytimg.com/vi/' + vid + '/default.jpg';
       return template.render(player, flashvars);
